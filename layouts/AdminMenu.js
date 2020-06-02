9977cdc5763861c32d54df6f50e18d0770f969e1
tuntikorn <tuntikornwisuttikul@gmail.com>
2020-06-02 18:01:37 +0700

    [Update] Header

--- layouts/AdminMenu.js
+++ layouts/AdminMenu.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Link from 'next/link'
+
 const AdminMenu = (props) => {
-    return <div style={{ width: 180, display: 'flex', flex: 0.25, backgroundColor: "lightgrey", position: 'fixed', top: 100, bottom: 50, left: 0 }}>
+    return <div style={{ width: 180, display: 'flex', flex: 0.25, backgroundColor: "lightgrey", position: 'fixed', top: 100, bottom: 40, left: 0 }}>
 
         <div style={{ display: 'flex', flex: 1, width: '100%', flexDirection: 'column', justifyContent: 'center' }}>
             <div style={{ height: 50 }}>
-
             </div>
             <div style={{ height: 50 }}>
             </div>
