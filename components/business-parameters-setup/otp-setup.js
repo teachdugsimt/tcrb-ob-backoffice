29e75063bab259708323ce7af3e1e7ee5515022c
sarun <sarun.p@tcrbank.com>
2020-06-09 19:07:49 +0700

    [Add] useState

--- components/business-parameters-setup/otp-setup.js
+++ components/business-parameters-setup/otp-setup.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input, Row, Col, Button } from 'antd'
 import styled from 'styled-components'
-
+import SimpleModal from '../simple-modal'
 const StyledInput = styled(Input)`
 
       background-color: unset !important;
@@ -15,14 +15,33 @@ const StyledInput = styled(Input)`
 
 `
 export default function OtpSetup() {
+  const [disableEditOtp, setDisableEditOtp] = useState(true)
+  const [visible, setVisble] = useState(false)
+  const [modalString, setModalString] = useState('')
+
+  const editOtpMaximum = () => {
+    if(disableEditOtp){
+      setDisableEditOtp(false)
+    }else{
+      setModalString(
+        <div>
+          <p>Changing OTP Maximum Retrying!!</p>
+          <p>From ...</p>
+          </div>
+      )
+    }
+  }
+  const changeOtpMaximum = ()=>{
+
+  }
   return (
     <div>
       <Row gutter={[8, 8]}>
         <Col span={8}>
-          <StyledInput prefix="OTP Maximum Retrying" suffix="Times" />
+          <StyledInput prefix="OTP Maximum Retrying" suffix="Times" readOnly={disableEditOtp}/>
         </Col>
         <Col span={6}>
-          <Button> Edit </Button>
+          <Button onClick={editOtpMaximum}> {disableEditOtp ? 'Edit':'Submit'} </Button>
         </Col>
       </Row>
       <Row gutter={[8, 8]}>
@@ -33,6 +52,14 @@ export default function OtpSetup() {
           <Button> Edit </Button>
         </Col>
       </Row>
+      <SimpleModal
+        onOk={()=> unlockOTP()}
+        onCancel={() => setVisble(false)}
+        okText="Confirm"
+        cancelText="Cancel"
+        modalString={modalString}
+        visible={visible}
+      />
 
     </div>
   )
