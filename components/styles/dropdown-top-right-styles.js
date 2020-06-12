a08b6a32fe667170fcd0d6ca0fa438b28f16ebb5
tuntikorn <tuntikornwisuttikul@gmail.com>
2020-06-12 17:49:05 +0700

    [Add] some styles

---
+++ components/styles/dropdown-top-right-styles.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components';
+import { useStores } from '../../hooks/use-stores'
+import { Select } from 'antd';
+const { Option } = Select;
+
+const width = () => {
+  const { versatileStore } = useStores()
+  return versatileStore.sidebarWidth
+}
+
+const DropdownContainer = styled.div`
+margin-right: ${({ theme }) => theme.spacing.medium}px;;
+display: flex;
+flex: 1;
+flex-direction: row;
+justify-content: flex-end;
+width: 100%;
+`
+
+export {
+  DropdownContainer,
+}
