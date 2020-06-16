6389032022ea082abc7c3269d9c4997fd93d125d
tuntikorn <tuntikornwisuttikul@gmail.com>
2020-06-16 17:37:09 +0700

    [Update] structor css

--- components/customer-services-menu/index.js
+++ components/customer-services-menu/index.js
@@ -5,7 +5,19 @@ import AccountUnbinding from './account-unbinding'
 const { TabPane } = Tabs;
 import { withTranslation } from '../../i18n'
 import { inject, observer } from 'mobx-react'
+import styled from 'styled-components';
 
+const MainTab = styled(Tabs)`
+    .ant-tabs-tab-active{
+      color: orange !important;
+    }
+    .ant-tabs-tab:hover{
+      color: orange !important;
+    }
+    .ant-tabs-ink-bar {
+      background: orange !important;
+    }
+`
 // export default function CustomerServicesMenu
 const CustomerServicesMenu =
   inject('customerServicesMenuStore')
@@ -17,14 +29,14 @@ const CustomerServicesMenu =
           <Spin spinning={customerServicesMenuStore.apiFetching} size="large" tip="Loading...">
             <Row flex={100}>
               <Col flex={100}>
-                <Tabs defaultActiveKey="1" destroyInactiveTabPane={true}>
-                  <TabPane tab={t("accountUnbinding")} key="1" >
+                <MainTab defaultActiveKey="1" destroyInactiveTabPane={true}>
+                  <TabPane tab={t("accountUnbinding")} key="1">
                     <AccountUnbinding />
                   </TabPane>
                   <TabPane tab={t("otpUnlock")} key="2" >
                     <OtpUnlocking />
                   </TabPane>
-                </Tabs>
+                </MainTab>
               </Col>
             </Row>
           </Spin>
