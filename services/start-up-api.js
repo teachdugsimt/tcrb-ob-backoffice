87f4e17f5ef03ea1b4155d128ae607083e12d42a
sarun <sarun.p@tcrbank.com>
2020-06-16 14:23:15 +0700

    [Delete] customer-services-menu-api in start up

--- services/start-up-api.js
+++ services/start-up-api.js
@@ -14,34 +14,5 @@ class StartupApi {
       return problem
     }
   }
-  getAccountInfo = async (params) => {
-    const api = create(Header)
-    const response = await api.get('api/backoffice/v1/otp/blacklist', params)
-    return response
-  }
-
-  getAccountInfoUnbinding = async (params) => {
-    const api = create(Header)
-    const response = await api.get('api/backoffice/v1/accounts', params)
-    return response
-  }
-
-  getAccountProductsInfo = async (params) => {
-    const api = create(Header)
-    const response = await api.get('api/backoffice/v1/accounts/partner-info', params)
-    return response
-  }
-
-  unlockOTPAccount = async (params) => {
-    const api = create(Header)
-    const response = await api.post('api/backoffice/v1/otp/unlock', params)
-    return response
-  }
-
-  unbindAccount = async (params) => {
-    const api = create(Header)
-    const response = await api.post('api/accounts/unbind', params)
-    return response
-  }
 }
 export default new StartupApi()
