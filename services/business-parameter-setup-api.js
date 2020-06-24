12d4eb9eca9dc8674758ce76ba1666a21e4fd7df
tuntikorn <tuntikornwisuttikul@gmail.com>
2020-06-24 10:54:04 +0700

    [Update] Header Structor

--- services/business-parameter-setup-api.js
+++ services/business-parameter-setup-api.js
@@ -8,9 +8,8 @@ class BusinessParameterSetupApi {
 
   getOtpValue = async (params) => {
     const api = create(Header())
-    console.log("header : ", Header())
     const response = await api.get('api/backoffice/v1/parameterstore', params)
-    console.log("response get OTP  :  ", response)
+    // console.log("response get OTP  :  ", response)
     return response
   }
 
@@ -31,35 +30,21 @@ class BusinessParameterSetupApi {
       rejectUnauthorized: false,
       requestCert: false,
     })
-    console.log("HTTPS AGENT : ", httpsAgent)
-    const response = await axios({
-      url: 'https://dwl8p0fxml-vpce-03ae60b10934425db.execute-api.ap-southeast-1.amazonaws.com/api/backoffice/v1/parameterstore',
-      // url: 'https://api-dev.onlinebanking-backoffice.com/api/backoffice/v1/parameterstore',
-      method: 'GET',
+    console.log("Param before call : ", params)
+    // const response = await axios.get("https://api-dev.onlinebanking-backoffice.com/api/backoffice/v1/parameterstore", {
+    const response = await axios.get("https://dwl8p0fxml-vpce-03ae60b10934425db.execute-api.ap-southeast-1.amazonaws.com/api/backoffice/v1/parameterstore", {
       params,
-      responseType: 'json',
       httpsAgent,
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        // 'x-apigw-api-id': 'dwl8p0fxml',
 
-        // 'Access-Control-Allow-Origin': "*",
-        'x-apigw-api-id': "dwl8p0fxml",
-        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-
-        // 'x-api-language': "TH",
-        // 'x-api-key': '',
-        // 'x-api-signature': 'a94ks02304ldfgkorm1234llgdmfk',
-        // 'uuid': 'YYYYYYYY',
-      },
-      timeout: 15000
-    }).then(data => {
-      console.log("AXIOS CALLLL : ", data)
-      return data
-    }).catch(err => {
-      console.log("AXIOS ERROR : ", err)
-      return err
+        // 'X-Forwarded-Proto': 'https',
+        // 'x-amzn-vpce-id': 'vpce-03ae60b10934425db',
+        // 'x-amzn-vpc-id': 'vpc-024c7d2df440a6d46',
+        // 'X-Forwarded-Port': '443'
+      }
     })
     return response
   }
