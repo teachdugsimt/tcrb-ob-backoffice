a93d1e4d5eb1a864fb5066ae5b0a19def544f142
sarun <sarun.p@tcrbank.com>
2020-07-14 18:45:25 +0700

    add codition menu render action and call refeash menu

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "735648c2ab05664646c3";
+/******/ 	var hotCurrentHash = "82ec06ef150517ae392d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
